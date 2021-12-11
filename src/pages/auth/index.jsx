ea11fbd43d35ad12f2a73fe214ba17083402ebf9
Eslim Daga <esliim9811@gmail.com>
2021-12-11 18:24:54 -0500

    Finish configuration Tailwind

---
+++ src/pages/auth/index.jsx
@@ -0,0 +1,9 @@
+const AuthPage = () => {
+  return (
+  <>
+    <h1>Hello World</h1>
+  </>
+  )
+}
+
+export default AuthPage;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans' : ['Quicksand', 'Helvetica', 'Arial', 'sans-serif'],
+        'mono' : ['Quicksand', 'Helvetica', 'Arial', 'sans-serif'],
+        'serif' : ['Quicksand', 'Helvetica', 'Arial', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
