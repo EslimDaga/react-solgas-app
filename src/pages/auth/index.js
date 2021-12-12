135abdfcdbeb69eaa3f0f3fee1b6b62329030a50
Eslim Daga <esliim9811@gmail.com>
2021-12-12 00:33:34 -0500

    Change name onsubmit

--- src/pages/auth/index.js
+++ src/pages/auth/index.js
@@ -1,10 +1,17 @@
-import { useState } from "react"
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { CenterLogo,LogoImage } from "../../components/common/Logo";
 import BackgroundImage from "../../assets/images/background-solgas.jpeg";
-import { EyeIcon, EyeOffIcon } from "@heroicons/react/solid";
+import { EyeIcon, EyeOffIcon, ExclamationCircleIcon } from "@heroicons/react/solid";
 import "../../assets/styles/css/auth/style.css";
 
 const AuthPage = () => {
+
+  const { register, handleSubmit, formState : { errors }} = useForm();
+
+  const onSubmitForm = e => {
+    console.log(e);
+  }
 
   const [passwordShow, setPasswordShow] = useState(false);
   const togglePasswordVisibility = () => {
@@ -15,8 +22,7 @@ const AuthPage = () => {
     <>
       <main>
         <section className="absolute w-full h-full">
-          <div
-            className="absolute top-0 w-full h-full bg-gray-900 bg-login"
+          <div className="absolute top-0 w-full h-full bg-gray-900 bg-login"
             style={{
               backgroundImage: `url(${BackgroundImage})`,
               backgroundSize: "100%",
@@ -33,53 +39,65 @@ const AuthPage = () => {
                     </CenterLogo>
                   </div>
                   <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
-                    <form>
+                    <form onSubmit={handleSubmit(onSubmitForm)}>
                       <div className="relative w-full mb-3">
                         <label
-                          className="block text-gray-700 text-base font-bold mb-2"
-                          htmlFor="grid-password"
+                          className={`block text-gray-700 text-base font-bold mb-2` + (errors.username ? " text-red-500" : "")}
                         >
                           Nombre de Usuario
                         </label>
                         <input
-                          type="email"
-                          className="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-base shadow focus:outline-none focus:ring w-full"
+                          type="text"
+                          name="username"
+                          className={`border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-base shadow focus:outline-none focus:ring w-full` + (errors.username ? " focus:border-2 border-rose-500 border-2 border-rose-500" : "")}
                           style={{ transition: "all .15s ease" }}
+                          {...register("username", {
+                            required: {
+                              value: true,
+                              message: "El nombre de usuario es requerido"
+                            }
+                          })}
                         />
+                        {errors.username && <span className="text-red-500 text-sm font-bold flex mt-1">{errors.username.message} <ExclamationCircleIcon className="text-red-500 h-5 w-5 ml-1"/></span>}
                       </div>
                       <div className="relative w-full mb-3">
                         <label
-                          className="block text-gray-700 text-base font-bold mb-2"
-                          htmlFor="grid-password"
+                          className={`block text-gray-700 text-base font-bold mb-2` + (errors.password ? " text-red-500" : "")}
                         >
                           Contraseña
                         </label>
                         <input
                           type={passwordShow ? "text" : "password"}
-                          className="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-base shadow focus:outline-none focus:ring w-full"
+                          name="password"
+                          className={`border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-base shadow focus:outline-none focus:ring w-full` + (errors.password ? " focus:border-2 border-rose-500 border-2 border-rose-500" : "")}
+                          style={{ transition: "all .15s ease" }}
+                          {...register("password", {
+                            required: {
+                              value: true,
+                              message: "La contraseña es requerida"
+                            }
+                          })}
+                        />
+                        {errors.password ? "" : <div onClick={togglePasswordVisibility}>
+                          {passwordShow ? <EyeIcon className="h-5 w-5 text-dark-500 float-right relative bottom-8 right-3"/> : <EyeOffIcon className="h-5 w-5 text-dark-500 float-right relative bottom-8 right-3"/>}
+                        </div>}
+                        {errors.password && <span className="text-red-500 text-sm font-bold flex mt-1">{errors.password.message} <ExclamationCircleIcon className="text-red-500 h-5 w-5 ml-1"/></span>}
+                      </div>
+                      <label className="inline-flex items-center cursor-pointer">
+                        <input
+                          id="customCheckLogin"
+                          type="checkbox"
+                          className="form-checkbox border-0 rounded text-gray-800 ml-1 w-5 h-5"
                           style={{ transition: "all .15s ease" }}
                         />
-                        <div onClick={togglePasswordVisibility}>
-                          {passwordShow ? <EyeIcon className="h-5 w-5 text-dark-500 float-right relative bottom-8 right-3"/> : <EyeOffIcon className="h-5 w-5 text-dark-500 float-right relative bottom-8 right-3"/>}
-                        </div>
-                      </div>
-                      <div>
-                        <label className="inline-flex items-center cursor-pointer">
-                          <input
-                            id="customCheckLogin"
-                            type="checkbox"
-                            className="form-checkbox border-0 rounded text-gray-800 ml-1 w-5 h-5"
-                            style={{ transition: "all .15s ease" }}
-                          />
-                          <span className="ml-2 text-base font-semibold text-gray-700">
-                            Mantenme conectado
-                          </span>
-                        </label>
-                      </div>
+                        <span className="ml-2 text-base font-semibold text-gray-700">
+                          Mantenme conectado
+                        </span>
+                      </label>
                       <div className="text-center mt-6">
                         <button
-                          className="bg-gray-900 text-white active:bg-gray-700 text-base font-bold px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full"
-                          type="button"
+                          className={`bg-gray-900 text-white active:bg-gray-700 text-base font-bold px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full` + (errors.password || errors.username ? " opacity-50 cursor-not-allowed" : "")}
+                          type="submit"
                           style={{ transition: "all .15s ease" }}
                         >
                           Iniciar Sesión
