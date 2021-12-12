b3084c86ee2c96eaeba92cf0b7c243c52af298c9
Eslim Daga <esliim9811@gmail.com>
2021-12-12 15:31:19 -0500

    Fix login and end

--- src/service/auth.js
+++ src/service/auth.js
@@ -2,35 +2,23 @@ import axios from "axios";
 import { api } from "../constants/global";
 
 export const login = async (data) => {
-  await axios.post(`${api}/token/obtain/`, {
-    username: data.username,
-    password: data.password,
-  }).then(r => {
-    console.log("Login response: ", r);
-    fechtData(data.username, r.data.access).then(res => {
-      const user = {
-        ...res,
-        token: r.data.access
-      };
-      console.log("Fetch data",user);
-      return user;
-    })
-  }).catch(err => {
-    console.log("Login Error", err);
-    return err;
+  //Obtain Token
+  const { username, password } = data;
+  const getToken = await axios.post(`${api}/token/obtain/`, {
+    username,
+    password
   });
-}
-
-export const fechtData = async (username, token) => {
-  await axios.get(`${api}/users/web/api/get-user/${username}/`, {
+  const { access } = getToken.data;
+  //Get User
+  const getUser = await axios.get(`${api}/users/web/api/get-user/${username}/`,{
     headers: {
-      Authorization: `JWT ${token}`
+      Authorization: `JWT ${access}`
     }
-  }).then(res => {
-    console.log("Res Data", res.data);
-    return res.data;
-  }).catch(error => {
-    console.log("Error fetchdata", error);
-    return error;
-  })
+  });
+  const userInfo = getUser.data;
+  const user = {
+    ...userInfo,
+    token : access
+  }
+  return user;
 }
