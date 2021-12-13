c3c8d77dd20c8cac475887d1692f69a35504a7d6
Eslim Daga <esliim9811@gmail.com>
2021-12-12 19:45:36 -0500

    Add route not found

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import AuthPage from "./pages/auth/";
 import EventPage from "./pages/events";
+import NotFound from "./pages/404";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route exact path="/" element={<Navigate to="/login"/>} />
         <Route exact path="/login" element={<AuthPage/>}/>
         <Route exact path="/events" element={<EventPage/>}/>
+        <Route path="*" element={<NotFound/>}/>
       </Routes>
     </Router>
   );
