63bd21cb331df509b8f9bca708c2c14893761892
Eslim Daga <esliim9811@gmail.com>
2021-12-12 19:21:27 -0500

    Add link react router dom

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import { Fragment } from "react";
 import { Popover, Transition } from "@headlessui/react";
 import { LogoHeader } from "./common/LogoHeader";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
+import { Link } from "react-router-dom";
 
 export default function Example() {
   return (
@@ -9,9 +10,9 @@ export default function Example() {
       <div className="mx-auto px-4 sm:px-6">
         <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
-            <a href="/">
+            <Link to="/login">
               <LogoHeader />
-            </a>
+            </Link>
           </div>
           <div className="-mr-2 -my-2 md:hidden">
             <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
@@ -20,9 +21,9 @@ export default function Example() {
           </div>
           <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
             <Popover.Group as="nav" className="hidden md:flex space-x-10">
-              <a href="/" className="text-base font-medium text-gray-500 hover:text-gray-900">
+              <Link to="/events" className="text-base font-medium text-gray-500 hover:text-gray-900">
                 Eventos
-              </a>
+              </Link>
               <a href="/" className="text-base font-medium text-gray-500 hover:text-gray-900">
                 Historial
               </a>
