c40608bcd189e9a330acb07813325612b1f6aeac
Eslim Daga <esliim9811@gmail.com>
2021-12-12 19:10:27 -0500

    Add route event

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AuthPage from "./pages/auth/";
+import EventPage from "./pages/events";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path="/login" element={<AuthPage/>}/>
+        <Route exact path="/events" element={<EventPage/>}/>
       </Routes>
     </Router>
   );
