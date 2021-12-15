185634307f19fea518093a189fc1600e372f8fdf
Eslim Daga Castro <esliim9811@gmail.com>
2021-12-15 10:47:15 -0500

    Add search and pagination table

--- src/components/events/EventTable.jsx
+++ src/components/events/EventTable.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
 import { getEvents } from "../../service/event";
+import { SearchCircleIcon } from "@heroicons/react/solid"
 
 const EventTable = () => {
   const [isLoading, setIsLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [search, setSearch] = useState("");
   const [events, setEvents] = useState([]);
 
   useEffect(() => {
@@ -11,15 +14,56 @@ const EventTable = () => {
       setEvents(events);
     })
   },[]);
+
+  const filteredEvents = () => {
+    if(search.length === 0){
+      return events.slice(currentPage, currentPage + 10);
+    }
+    //If there is something in the text box
+    const filtered = events.filter(eve => eve.unitid.toLowerCase().includes(search));
+    return filtered.slice(currentPage, currentPage + 10);
+  }
+
+  const nextPage = () => {
+    if(events.filter(eve => eve.unitid.toLowerCase().includes(search)).length > currentPage + 10){
+      setCurrentPage(currentPage + 10);
+    }
+  }
+
+  const prevPage = () => {
+    if(currentPage > 0) {
+      setCurrentPage(currentPage - 10);
+    }
+  }
+
+  const onSearchChange = ({target}) => {
+    setCurrentPage(0);
+    setSearch(target.value);
+  }
+
   return (
   <>
     <div className="bg-white">
       <div className="pt-1">
         <div className="mx-auto px-4 flex items-center space-x-2 sm:px-6 lg:max-w-full lg:px-6">
-          <section className="antialiased bg-gray-100 text-gray-600 w-full">
+          <section className="antialiased text-gray-600 w-full">
             <div className="flex flex-col justify-center">
-              <div className="w-full mx-auto bg-white shadow-lg rounded-lg border border-gray-200">
-                <div className="pr-3 pl-3">
+              <div className="container flex items-center pb-3">
+                <div className="relative">
+                  <div className="absolute top-4 left-3">
+                    <SearchCircleIcon className="h-6 w-6"/>
+                  </div>
+                  <input
+                    type="text"
+                    className="bg-gray-100 h-14 w-96 pl-12 pr-20 rounded-lg z-0 focus:shadow focus:outline-none font-bold"
+                    placeholder="Buscar por Unidad"
+                    value={search}
+                    onChange={onSearchChange}
+                  />
+                </div>
+              </div>
+              <div className="w-full mx-auto bg-white rounded-lg">
+                <div className="lg:pr-3 sm:pr-1 lg:pl-3 sm:pl-1 pb-3">
                   <div className="flex flex-col">
                     <div className="-my-2 overflow-x-auto sm:-mx-3 lg:-mx-8">
                       <div className="py-2 align-middle inline-block min-w-full sm:px-0 lg:px-5">
@@ -72,7 +116,7 @@ const EventTable = () => {
                               </tr>
                             </thead>
                             <tbody className="bg-white divide-y divide-gray-200">
-                              {events.map((event) => (
+                              {filteredEvents().map((event) => (
                                 <tr key={event.id}>
                                   <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{event.provider}</td>
                                   <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{event.logistic_operator}</td>
@@ -92,6 +136,20 @@ const EventTable = () => {
                   </div>
                 </div>
               </div>
+            </div>
+            <div className="mb-3">
+              <button
+                className="bg-blue-900 text-white font-bold py-2 px-4 rounded mr-2"
+                onClick={prevPage}
+              >
+                Anterior
+              </button>
+              <button
+                className="bg-blue-900 text-white font-bold py-2 px-4 rounded"
+                onClick={nextPage}
+              >
+                Siguiente
+              </button>
             </div>
           </section>
         </div>
