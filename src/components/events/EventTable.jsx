099e1f824d8793263b2bbfad07fd2806ff93912d
Eslim Daga Castro <esliim9811@gmail.com>
2021-12-15 16:16:01 -0500

    Fix table

--- src/components/events/EventTable.jsx
+++ src/components/events/EventTable.jsx
@@ -50,14 +50,14 @@ const EventTable = () => {
         <div className="mx-auto px-4 flex items-center space-x-2 sm:px-6 lg:max-w-full lg:px-6">
           <section className="antialiased text-gray-600 w-full">
             <div className="flex flex-col justify-center">
-              <div className="container flex items-center pb-3">
+              <div className="items-center pb-3 sm:relative lg:flex">
                 <div className="relative">
                   <div className="absolute top-4 left-3">
                     <SearchCircleIcon className="h-6 w-6"/>
                   </div>
                   <input
                     type="text"
-                    className="bg-gray-100 h-14 w-96 pl-12 pr-20 rounded-lg z-0 focus:shadow focus:outline-none font-bold"
+                    className="bg-gray-100 h-14 w-full pl-12 pr-20 rounded-lg z-0 focus:shadow focus:outline-none font-bold"
                     placeholder="Buscar por Unidad"
                     value={search}
                     onChange={onSearchChange}
