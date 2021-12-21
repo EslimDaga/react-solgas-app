45d65a4ed9e856fd810a9e4a5da3652c52a2f63f
Eslim Daga Castro <esliim9811@gmail.com>
2021-12-21 16:02:52 -0500

    Add history page link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import AuthPage from "./pages/auth/";
 import EventPage from "./pages/events";
+import HistoryPage from "./pages/history";
 import NotFound from "./pages/404";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Route exact path="/" element={<Navigate to="/login"/>} />
         <Route exact path="/login" element={<AuthPage/>}/>
         <Route exact path="/events" element={<EventPage/>}/>
+        <Route exact path="/history" element={<HistoryPage/>}/>
         <Route path="*" element={<NotFound/>}/>
       </Routes>
     </Router>
