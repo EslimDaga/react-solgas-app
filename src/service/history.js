560e8c0dabac5694dbd405efef6f172ed58fa2fc
Eslim Daga Castro <esliim9811@gmail.com>
2021-12-21 18:20:13 -0500

    Add getSearchEvent

--- src/service/history.js
+++ src/service/history.js
@@ -11,3 +11,14 @@ export const getUnits = async () => {
   });
   return response.data;
 }
+
+export const getSearchEvents = async(initial_date,final_date,unit_name) => {
+  const token = cache.getItem("user").token;
+  const response = await axios.get(`${api}/control/web/api/search-events/${initial_date}/${final_date}/${unit_name}/`,{
+    headers: {
+      "Authorization": `JWT ${token}`
+    }
+  });
+  console.log(response.data);
+  return response.data;
+}
