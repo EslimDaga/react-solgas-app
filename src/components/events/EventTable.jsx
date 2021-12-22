b40aed9c74a85b43b2fc8633367c0232b4bc55f6
Eslim Daga Castro <esliim9811@gmail.com>
2021-12-22 10:06:13 -0500

    Fix dropwpown

--- src/components/events/EventTable.jsx
+++ src/components/events/EventTable.jsx
@@ -363,15 +363,15 @@ const EventTable = () => {
                                       <ul className="dropdown-menu absolute hidden text-gray-700 pt-1 z-50">
                                         <li className="">
                                           <button
-                                            className="rounded-t font-bold bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
+                                            className="font-bold bg-gray-200 hover:bg-gray-400 py-2 px-6 block whitespace-no-wrap"
                                             onClick={() => openModal(event.id,true)}
                                           >
-                                          Detalles&nbsp;&nbsp;
+                                          Detalles
                                           </button>
                                         </li>
                                         <li className="">
                                           <button
-                                            className="font-bold bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
+                                            className="font-bold bg-gray-200 hover:bg-gray-400 py-2 px-5 block whitespace-no-wrap"
                                             onClick={() => openModalImages(event.id,true)}
                                           >
                                           Imágenes
