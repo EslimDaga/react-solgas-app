a6f88b422cefdf0cc872adb5af59c46d90e43026
Eslim Daga Castro <esliim9811@gmail.com>
2021-12-30 12:05:16 -0500

    Add onclick modals

--- src/components/events/EventTable.jsx
+++ src/components/events/EventTable.jsx
@@ -364,6 +364,7 @@ const EventTable = () => {
                                         <li className="">
                                           <button
                                             className="font-bold bg-gray-200 hover:bg-gray-400 py-2 px-6 block whitespace-no-wrap"
+                                            onClick={() => openModal(event.id,true)}
                                           >
                                           Detalles
                                           </button>
@@ -371,6 +372,7 @@ const EventTable = () => {
                                         <li className="">
                                           <button
                                             className="font-bold bg-gray-200 hover:bg-gray-400 py-2 px-5 block whitespace-no-wrap"
+                                            onClick={() => openModalImages(event.id,true)}
                                           >
                                           Imágenes
                                           </button>
