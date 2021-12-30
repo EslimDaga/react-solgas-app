172c685a8aa4aacd1ed3d6b37ceccc92197c7286
Eslim Daga Castro <esliim9811@gmail.com>
2021-12-30 11:44:08 -0500

    Add file private route
    Add file private route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import EventPage from "./pages/events";
 import HistoryPage from "./pages/history";
 import NotFound from "./pages/404";
 import Logout from "./components/common/Logout";
+import PrivateRoute from "./components/common/PrivateRoute";
 
 function App() {
   return (
@@ -11,9 +12,11 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Navigate to="/login"/>} />
         <Route exact path="/login" element={<AuthPage/>}/>
-        <Route exact path="/logout" element={<Logout/>}/>
-        <Route exact path="/events" element={<EventPage/>}/>
-        <Route exact path="/history" element={<HistoryPage/>}/>
+        <Route element={<PrivateRoute />}>
+          <Route exact path="/events" element={<EventPage/>}/>
+          <Route exact path="/history" element={<HistoryPage/>}/>
+          <Route exact path="/logout" element={<Logout/>}/>
+        </Route>
         <Route path="*" element={<NotFound/>}/>
       </Routes>
     </Router>
