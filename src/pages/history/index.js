fd33ce56adac3781405815e34f582265fd8447e6
Eslim Daga Castro <esliim9811@gmail.com>
2021-12-30 12:25:41 -0500

    Add open modal and open modal images

--- src/pages/history/index.js
+++ src/pages/history/index.js
@@ -6,10 +6,15 @@ import Header from "../../components/Header";
 import Breadcrumb from "../../components/common/Breadcrumb";
 import ReactDatePicker from "react-datepicker";
 import { ToastContainer, toast } from "react-toastify";
+import { Carousel } from "react-responsive-carousel";
 import moment from "moment";
 import { SearchCircleIcon } from "@heroicons/react/outline";
 import "react-datepicker/dist/react-datepicker.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "../../assets/styles/css/history/style.css";
+import { BadgeCheckIcon } from "@heroicons/react/solid";
+import { getEventById } from "../../service/event";
+import { api } from "../../constants/global";
 
 const HistoryPage = () => {
   const { handleSubmit, formState : {errors}, control } = useForm();
@@ -18,6 +23,9 @@ const HistoryPage = () => {
   const [search, setSearch] = useState("");
   const [searchEvents, setSearchEvents] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [event, setEvent] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [showModalImages, setShowModalImages] = useState(false);
 
   useEffect(() => {
     getUnits().then(units => {
@@ -58,6 +66,24 @@ const HistoryPage = () => {
     setSearch(target.value);
   }
 
+  const openModal = (id,state) => {
+    getEventById(id).then(event => {
+      setEvent(event);
+      setShowModal(state);
+    }).catch(e => {
+      console.log(e);
+    })
+  }
+
+  const openModalImages = (id,state) => {
+    getEventById(id).then(event => {
+      setEvent(event);
+      setShowModalImages(state);
+    }).catch(e => {
+      console.log(e);
+    })
+  }
+
   const onSubmitForm = async(data) => {
     setLoading(true);
     const unit_name_value = data.unit_name.value;
@@ -86,6 +112,200 @@ const HistoryPage = () => {
   <>
     <Header/>
     <Breadcrumb title="Historial"/>
+    {showModal ? (
+      <>
+        <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+          <div className="relative w-auto my-6 mx-auto max-w-3xl">
+            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+              <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                <h3 className="text-1xl font-semibold self-center">
+                  Detalles del Evento
+                </h3>
+                <button
+                  className="p-1 ml-auto bg-transparent border-0 text-gray-900 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                  onClick={() => setShowModal(false)}
+                >
+                  <span className="bg-transparent text-gray-900 h-6 w-6 text-xl block outline-none focus:outline-none">
+                    x
+                  </span>
+                </button>
+              </div>
+              <div className="relative p-5 flex-auto">
+                <div className="w-full bg-gray-100 flex">
+                  <div className="bg-white rounded-lg shadow-sm">
+                    <div className="flex flex-col mt-5 gap-7 text-sm">
+                      <div className="bg-blue-900 flex justify-between items-center p-3 rounded-md shadow-sm">
+                        <div className="flex justify-start items-center gap-2">
+                          <BadgeCheckIcon className="h-8 w-8 text-yellow-500"/>
+                          <div>
+                            <p className="text-white font-bold tracking-wider">Operador Logistico</p>
+                            <p className="text-white font-bold">{event.logistic_operator}</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="flex flex-col mt-5 gap-7 text-sm">
+                      <div className="bg-blue-900 flex justify-between items-center p-3 rounded-md shadow-sm">
+                        <div className="flex justify-start items-center gap-2">
+                          <BadgeCheckIcon className="h-8 w-8 text-yellow-500"/>
+                          <div>
+                            <p className="text-white font-bold tracking-wider">Placa</p>
+                            <p className="text-white font-bold">{event.unitid}</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="flex flex-col mt-5 gap-7 text-sm">
+                      <div className="bg-blue-900 flex justify-between items-center p-3 rounded-md shadow-sm">
+                        <div className="flex justify-start items-center gap-2">
+                          <BadgeCheckIcon className="h-8 w-8 text-yellow-500"/>
+                          <div>
+                            <p className="text-white font-bold tracking-wider">Tipo de Servicio</p>
+                            <p className="text-white font-bold">{event.type_of_service}</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="flex flex-col mt-5 gap-7 text-sm">
+                      <div className="bg-blue-900 flex justify-between items-center p-3 rounded-md shadow-sm">
+                        <div className="flex justify-start items-center gap-2">
+                          <BadgeCheckIcon className="h-8 w-8 text-yellow-500"/>
+                          <div>
+                            <p className="text-white font-bold tracking-wider">Checkpoint</p>
+                            <p className="text-white font-bold">{event.checkpoint}</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="flex flex-col mt-5 gap-7 text-sm">
+                      <div className="bg-blue-900 flex justify-between items-center p-3 rounded-md shadow-sm">
+                        <div className="flex justify-start items-center gap-2">
+                          <BadgeCheckIcon className="h-8 w-8 text-yellow-500"/>
+                          <div>
+                            <p className="text-white font-bold tracking-wider">Conductor</p>
+                            <p className="text-white font-bold">{event.driver_fullname}</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="flex flex-col mt-5 gap-7 text-sm">
+                      <div className="bg-blue-900 flex justify-between items-center p-3 rounded-md shadow-sm">
+                        <div className="flex justify-start items-center gap-2">
+                          <BadgeCheckIcon className="h-8 w-8 text-yellow-500"/>
+                          <div>
+                            <p className="text-white font-bold tracking-wider">Fecha de Creación</p>
+                            <p className="text-white font-bold">{event.datetime}</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="flex flex-col mt-5 gap-7 text-sm">
+                      <div className="bg-blue-900 flex justify-between items-center p-3 rounded-md shadow-sm">
+                        <div className="flex justify-start items-center gap-2">
+                          <BadgeCheckIcon className="h-8 w-8 text-yellow-500"/>
+                          <div>
+                            <p className="text-white font-bold tracking-wider">Puntaje</p>
+                            <p className="text-white font-bold">{event.game_score}</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="flex flex-col mt-5 gap-7 text-sm">
+                      <div className="bg-blue-900 flex justify-between items-center p-3 rounded-md shadow-sm">
+                        <div className="flex justify-start items-center gap-2">
+                          <BadgeCheckIcon className="h-8 w-8 text-yellow-500"/>
+                          <div>
+                            <p className="text-white font-bold tracking-wider">Estado de Ruta</p>
+                            <p className="text-white font-bold">{event.route_status}</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+      </>
+    ) : null}
+    {showModalImages ? (
+      <>
+        <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+          <div className="relative w-auto my-6 mx-auto max-w-3xl">
+            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+              <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                <h3 className="text-1xl font-semibold self-center">
+                  Imagenes del Evento
+                </h3>
+                <button
+                  className="p-1 ml-auto bg-transparent border-0 text-gray-900 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                  onClick={() => setShowModalImages(false)}
+                >
+                  <span className="bg-transparent text-gray-900 h-6 w-6 text-xl block outline-none focus:outline-none">
+                    x
+                  </span>
+                </button>
+              </div>
+              <div className="relative p-5 flex-auto">
+                <div className="w-full bg-gray-100 flex">
+                  <div className="bg-white rounded-lg shadow-sm">
+                    <Carousel autoPlay width={500} dynamicHeight={20} thumbWidth={35}>
+                      <div>
+                        <img src={api + JSON.parse(event.images).url1} alt={api + JSON.parse(event.images).url1}/>
+                        <p className="legend">Selfie del Conductor</p>
+                      </div>
+                      <div>
+                        <img src={api + JSON.parse(event.images).url2} alt={api + JSON.parse(event.images).url2}/>
+                        <p className="legend">Extintor</p>
+                      </div>
+                      <div>
+                        <img src={api + JSON.parse(event.images).url3} alt={api + JSON.parse(event.images).url3}/>
+                        <p className="legend">Delantero Izquierdo</p>
+                      </div>
+                      <div>
+                        <img src={api + JSON.parse(event.images).url4} alt={api + JSON.parse(event.images).url4}/>
+                        <p className="legend">Delantero Derecho</p>
+                      </div>
+                      <div>
+                        <img src={api + JSON.parse(event.images).url5} alt={api + JSON.parse(event.images).url5}/>
+                        <p className="legend">Posterior Izquiera</p>
+                      </div>
+                      <div>
+                        <img src={api + JSON.parse(event.images).url6} alt={api + JSON.parse(event.images).url6}/>
+                        <p className="legend">Posterior Derecha</p>
+                      </div>
+                      <div>
+                        <img src={api + JSON.parse(event.images).url7} alt={api + JSON.parse(event.images).url7}/>
+                        <p className="legend">Toma frontal de la unidad</p>
+                      </div>
+                      <div>
+                        <img src={api + JSON.parse(event.images).url8} alt={api + JSON.parse(event.images).url8}/>
+                        <p className="legend">Toma posterior de la unidad</p>
+                      </div>
+                      <div>
+                        <img src={api + JSON.parse(event.images).url9} alt={api + JSON.parse(event.images).url9}/>
+                        <p className="legend">Luces delanteras</p>
+                      </div>
+                      <div>
+                        <img src={api + JSON.parse(event.images).url10} alt={api + JSON.parse(event.images).url10}/>
+                        <p className="legend">Luces posteriores</p>
+                      </div>
+                      <div>
+                        <img src={api + JSON.parse(event.images).url11} alt={api + JSON.parse(event.images).url11}/>
+                        <p className="legend">Valvula interna</p>
+                      </div>
+                    </Carousel>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+      </>
+    ) : null}
     <div className="bg-white">
       <div className="pt-1">
         <div className="mx-auto px-4 flex items-center space-x-2 sm:px-6 lg:max-w-full lg:px-6">
@@ -290,6 +510,7 @@ const HistoryPage = () => {
                                                   <li className="">
                                                     <button
                                                       className="font-bold bg-gray-200 hover:bg-gray-400 py-2 px-6 block whitespace-no-wrap"
+                                                      onClick={() => openModal(event.id,true)}
                                                     >
                                                     Detalles
                                                     </button>
@@ -297,6 +518,7 @@ const HistoryPage = () => {
                                                   <li className="">
                                                     <button
                                                       className="font-bold bg-gray-200 hover:bg-gray-400 py-2 px-5 block whitespace-no-wrap"
+                                                      onClick={() => openModalImages(event.id,true)}
                                                     >
                                                     Imágenes
                                                     </button>
@@ -330,6 +552,7 @@ const HistoryPage = () => {
                                                       <li className="">
                                                         <button
                                                           className="font-bold bg-gray-200 hover:bg-gray-400 py-2 px-6 block whitespace-no-wrap"
+                                                          onClick={() => openModal(event.id,true)}
                                                         >
                                                         Detalles
                                                         </button>
@@ -337,6 +560,7 @@ const HistoryPage = () => {
                                                       <li className="">
                                                         <button
                                                           className="font-bold bg-gray-200 hover:bg-gray-400 py-2 px-5 block whitespace-no-wrap"
+                                                          onClick={() => openModalImages(event.id,true)}
                                                         >
                                                         Imágenes
                                                         </button>
