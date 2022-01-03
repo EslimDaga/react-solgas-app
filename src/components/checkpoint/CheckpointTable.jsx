41bef76fb0c4fb4e6c3c01dd159af8249a660767
Eslim Daga Castro <esliim9811@gmail.com>
2022-01-03 16:46:46 -0500

    Remove px

--- src/components/checkpoint/CheckpointTable.jsx
+++ src/components/checkpoint/CheckpointTable.jsx
@@ -137,7 +137,7 @@ const CheckpointTable = () => {
                                           </button>
                                         </li>
                                         <li className="">
-                                          <button className="font-bold bg-gray-200 hover:bg-gray-400 py-2 px-5 block whitespace-no-wrap">
+                                          <button className="font-bold bg-gray-200 hover:bg-gray-400 py-2 px-6 block whitespace-no-wrap">
                                             Imágenes
                                           </button>
                                         </li>
