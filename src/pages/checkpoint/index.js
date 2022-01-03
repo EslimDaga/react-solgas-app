fa33da3b1bdf92b644d847b915a0f44cc55c0c69
Eslim Daga Castro <esliim9811@gmail.com>
2022-01-03 11:59:05 -0500

    Add checkpoint

---
+++ src/pages/checkpoint/index.js
@@ -0,0 +1,13 @@
+import Header from "../../components/Header";
+import Breadcrumb from "../../components/common/Breadcrumb";
+
+const Checkpoint = () => {
+  return (
+    <>
+      <Header />
+      <Breadcrumb title="Checkpoint" />
+    </>
+  )
+}
+
+export default Checkpoint;
