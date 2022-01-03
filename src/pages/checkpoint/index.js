fa23d25db082ff3793bcfa4ea695d796c5d73c1d
Eslim Daga Castro <esliim9811@gmail.com>
2022-01-03 16:12:11 -0500

    Add checkpointtable

--- src/pages/checkpoint/index.js
+++ src/pages/checkpoint/index.js
@@ -1,11 +1,13 @@
 import Header from "../../components/Header";
 import Breadcrumb from "../../components/common/Breadcrumb";
+import CheckpointTable from "../../components/checkpoint/CheckpointTable";
 
 const Checkpoint = () => {
   return (
     <>
       <Header />
       <Breadcrumb title="Checkpoint" />
+      <CheckpointTable />
     </>
   )
 }
