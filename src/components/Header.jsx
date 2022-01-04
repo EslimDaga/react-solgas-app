aa627e9fbb3fc54e2bc2e893d515264881b9248b
Eslim Daga Castro <esliim9811@gmail.com>
2022-01-04 15:35:57 -0500

    Add route

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,19 +21,34 @@ export default function Example() {
           </div>
           <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
             <Popover.Group as="nav" className="hidden md:flex space-x-10">
-              <Link to="/events" className="text-base font-medium text-gray-500 hover:text-gray-900">
+              <Link
+                to="/events"
+                className="text-base font-medium text-gray-500 hover:text-gray-900"
+              >
                 Eventos
               </Link>
-              <Link to="/history" className="text-base font-medium text-gray-500 hover:text-gray-900">
+              <Link
+                to="/history"
+                className="text-base font-medium text-gray-500 hover:text-gray-900"
+              >
                 Historial
               </Link>
-              <Link to="/checkpoint" className="text-base font-medium text-gray-500 hover:text-gray-900">
+              <Link
+                to="/checkpoint"
+                className="text-base font-medium text-gray-500 hover:text-gray-900"
+              >
                 Checkpoint
               </Link>
-              <a href="/" className="text-base font-medium text-gray-500 hover:text-gray-900">
+              <a
+                href="/"
+                className="text-base font-medium text-gray-500 hover:text-gray-900"
+              >
                 Conductores
               </a>
-              <a href="/" className="text-base font-medium text-gray-500 hover:text-gray-900">
+              <a
+                href="/"
+                className="text-base font-medium text-gray-500 hover:text-gray-900"
+              >
                 Unidades
               </a>
             </Popover.Group>
@@ -41,7 +56,8 @@ export default function Example() {
               to="/logout"
               className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-blue-900"
             >
-              <LogoutIcon className="w-5 h-5 mr-2"/>Cerrar Sesión
+              <LogoutIcon className="w-5 h-5 mr-2" />
+              Cerrar Sesión
             </Link>
           </div>
         </div>
@@ -55,7 +71,10 @@ export default function Example() {
         leaveFrom="opacity-100 scale-100"
         leaveTo="opacity-0 scale-95"
       >
-        <Popover.Panel focus className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
+        <Popover.Panel
+          focus
+          className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+        >
           <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
             <div className="pt-5 pb-6 px-5">
               <div className="flex items-center justify-between">
@@ -71,19 +90,34 @@ export default function Example() {
             </div>
             <div className="py-6 px-5 space-y-6">
               <div className="grid grid-cols-2 gap-y-4 gap-x-8">
-                <Link to="/events" className="text-base font-medium text-gray-900 hover:text-gray-700">
+                <Link
+                  to="/events"
+                  className="text-base font-medium text-gray-900 hover:text-gray-700"
+                >
                   Eventos
                 </Link>
-                <Link to="/history" className="text-base font-medium text-gray-900 hover:text-gray-700">
+                <Link
+                  to="/history"
+                  className="text-base font-medium text-gray-900 hover:text-gray-700"
+                >
                   Historial
                 </Link>
-                <a href="/" className="text-base font-medium text-gray-900 hover:text-gray-700">
+                <Link
+                  to="/checkpoint"
+                  className="text-base font-medium text-gray-900 hover:text-gray-700"
+                >
                   Checkpoint
-                </a>
-                <a href="/" className="text-base font-medium text-gray-900 hover:text-gray-700">
+                </Link>
+                <a
+                  href="/"
+                  className="text-base font-medium text-gray-900 hover:text-gray-700"
+                >
                   Conductores
                 </a>
-                <a href="/" className="text-base font-medium text-gray-900 hover:text-gray-700">
+                <a
+                  href="/"
+                  className="text-base font-medium text-gray-900 hover:text-gray-700"
+                >
                   Unidades
                 </a>
               </div>
@@ -92,7 +126,8 @@ export default function Example() {
                   to="/logout"
                   className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-blue-900"
                 >
-                  <LogoutIcon className="w-5 h-5 mr-2"/>Cerrar Sesión
+                  <LogoutIcon className="w-5 h-5 mr-2" />
+                  Cerrar Sesión
                 </Link>
               </div>
             </div>
@@ -100,5 +135,5 @@ export default function Example() {
         </Popover.Panel>
       </Transition>
     </Popover>
-  )
+  );
 }
