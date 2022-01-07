32355e3d4aee8f8d81b6334cb36bb2fd70a846ed
Eslim Daga Castro <58963865+EslimDaga@users.noreply.github.com>
2022-01-07 12:15:08 -0500

    Refactor component

--- src/components/events/EventTable.jsx
+++ src/components/events/EventTable.jsx
@@ -1,11 +1,11 @@
 import { Fragment, useEffect, useState } from "react";
 import { getEvents, getEventById } from "../../service/event";
-import { api } from "../../constants/global";
-import { SearchCircleIcon, BadgeCheckIcon, ChevronDownIcon } from "@heroicons/react/solid";
-import { Carousel } from "react-responsive-carousel";
+import { SearchCircleIcon, ChevronDownIcon } from "@heroicons/react/solid";
+import ModalDetails from "../common/ModalDetails";
+import ModalImages from "../common/ModalImages";
+import { Menu, Transition } from "@headlessui/react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "../../assets/styles/css/events/style.css";
-import { Menu, Transition } from "@headlessui/react";
 
 const EventTable = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -87,134 +87,7 @@ const EventTable = () => {
                     </span>
                   </button>
                 </div>
-                <div className="relative p-5 flex-auto">
-                  <div className="w-full bg-gray-100 flex">
-                    <div className="bg-white rounded-lg shadow-sm">
-                      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-10">
-                        <div className="flex flex-col sm:mt-0 gap-7 text-sm">
-                          <div className="bg-blue-900 flex justify-between items-center p-3 rounded-md shadow-sm">
-                            <div className="flex justify-start items-center gap-2">
-                              <BadgeCheckIcon className="h-8 w-8 text-yellow-500" />
-                              <div>
-                                <p className="text-white font-bold tracking-wider">
-                                  Operador Logistico
-                                </p>
-                                <p className="text-white font-bold">
-                                  {event.logistic_operator}
-                                </p>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                        <div className="flex flex-col sm:mt-0 gap-7 text-sm">
-                          <div className="bg-blue-900 flex justify-between items-center p-3 rounded-md shadow-sm">
-                            <div className="flex justify-start items-center gap-2">
-                              <BadgeCheckIcon className="h-8 w-8 text-yellow-500" />
-                              <div>
-                                <p className="text-white font-bold tracking-wider">
-                                  Placa
-                                </p>
-                                <p className="text-white font-bold">
-                                  {event.unitid}
-                                </p>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                        <div className="flex flex-col sm:mt-0 gap-7 text-sm">
-                          <div className="bg-blue-900 flex justify-between items-center p-3 rounded-md shadow-sm">
-                            <div className="flex justify-start items-center gap-2">
-                              <BadgeCheckIcon className="h-8 w-8 text-yellow-500" />
-                              <div>
-                                <p className="text-white font-bold tracking-wider">
-                                  Tipo de Servicio
-                                </p>
-                                <p className="text-white font-bold">
-                                  {event.type_of_service}
-                                </p>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                        <div className="flex flex-col sm:mt-0 gap-7 text-sm">
-                          <div className="bg-blue-900 flex justify-between items-center p-3 rounded-md shadow-sm">
-                            <div className="flex justify-start items-center gap-2">
-                              <BadgeCheckIcon className="h-8 w-8 text-yellow-500" />
-                              <div>
-                                <p className="text-white font-bold tracking-wider">
-                                  Fecha de Creación
-                                </p>
-                                <p className="text-white font-bold">
-                                  {event.datetime}
-                                </p>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                        <div className="flex flex-col sm:mt-0 gap-7 text-sm">
-                          <div className="bg-blue-900 flex justify-between items-center p-3 rounded-md shadow-sm">
-                            <div className="flex justify-start items-center gap-2">
-                              <BadgeCheckIcon className="h-8 w-8 text-yellow-500" />
-                              <div>
-                                <p className="text-white font-bold tracking-wider">
-                                  Puntaje
-                                </p>
-                                <p className="text-white font-bold">
-                                  {event.game_score}
-                                </p>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                        <div className="flex flex-col sm:mt-0 gap-7 text-sm">
-                          <div className="bg-blue-900 flex justify-between items-center p-3 rounded-md shadow-sm">
-                            <div className="flex justify-start items-center gap-2">
-                              <BadgeCheckIcon className="h-8 w-8 text-yellow-500" />
-                              <div>
-                                <p className="text-white font-bold tracking-wider">
-                                  Estado de Ruta
-                                </p>
-                                <p className="text-white font-bold">
-                                  {event.route_status}
-                                </p>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                        <div className="flex flex-col sm:mt-0 gap-7 text-sm">
-                          <div className="bg-blue-900 flex justify-between items-center p-3 rounded-md shadow-sm">
-                            <div className="flex justify-start items-center gap-2">
-                              <BadgeCheckIcon className="h-8 w-8 text-yellow-500" />
-                              <div>
-                                <p className="text-white font-bold tracking-wider">
-                                  Conductor
-                                </p>
-                                <p className="text-white font-bold">
-                                  {event.driver_fullname}
-                                </p>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                        <div className="flex flex-col sm:mt-0 gap-7 text-sm">
-                          <div className="bg-blue-900 flex justify-between items-center p-3 rounded-md shadow-sm">
-                            <div className="flex justify-start items-center gap-2">
-                              <BadgeCheckIcon className="h-8 w-8 text-yellow-500" />
-                              <div>
-                                <p className="text-white font-bold tracking-wider">
-                                  Checkpoint
-                                </p>
-                                <p className="text-white font-bold">
-                                  {event.checkpoint}
-                                </p>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+                <ModalDetails event={event} />
               </div>
             </div>
           </div>
@@ -239,96 +112,7 @@ const EventTable = () => {
                     </span>
                   </button>
                 </div>
-                <div className="relative flex-auto">
-                  <div className="w-full bg-gray-100 flex">
-                    <div className="bg-white rounded-lg shadow-sm">
-                      <Carousel
-                        autoPlay
-                        width={500}
-                        dynamicHeight={20}
-                        thumbWidth={35}
-                      >
-                        <div>
-                          <img
-                            src={api + JSON.parse(event.images).url1}
-                            alt={api + JSON.parse(event.images).url1}
-                          />
-                          <p className="legend">Selfie del Conductor</p>
-                        </div>
-                        <div>
-                          <img
-                            src={api + JSON.parse(event.images).url2}
-                            alt={api + JSON.parse(event.images).url2}
-                          />
-                          <p className="legend">Extintor</p>
-                        </div>
-                        <div>
-                          <img
-                            src={api + JSON.parse(event.images).url3}
-                            alt={api + JSON.parse(event.images).url3}
-                          />
-                          <p className="legend">Delantero Izquierdo</p>
-                        </div>
-                        <div>
-                          <img
-                            src={api + JSON.parse(event.images).url4}
-                            alt={api + JSON.parse(event.images).url4}
-                          />
-                          <p className="legend">Delantero Derecho</p>
-                        </div>
-                        <div>
-                          <img
-                            src={api + JSON.parse(event.images).url5}
-                            alt={api + JSON.parse(event.images).url5}
-                          />
-                          <p className="legend">Posterior Izquiera</p>
-                        </div>
-                        <div>
-                          <img
-                            src={api + JSON.parse(event.images).url6}
-                            alt={api + JSON.parse(event.images).url6}
-                          />
-                          <p className="legend">Posterior Derecha</p>
-                        </div>
-                        <div>
-                          <img
-                            src={api + JSON.parse(event.images).url7}
-                            alt={api + JSON.parse(event.images).url7}
-                          />
-                          <p className="legend">Toma frontal de la unidad</p>
-                        </div>
-                        <div>
-                          <img
-                            src={api + JSON.parse(event.images).url8}
-                            alt={api + JSON.parse(event.images).url8}
-                          />
-                          <p className="legend">Toma posterior de la unidad</p>
-                        </div>
-                        <div>
-                          <img
-                            src={api + JSON.parse(event.images).url9}
-                            alt={api + JSON.parse(event.images).url9}
-                          />
-                          <p className="legend">Luces delanteras</p>
-                        </div>
-                        <div>
-                          <img
-                            src={api + JSON.parse(event.images).url10}
-                            alt={api + JSON.parse(event.images).url10}
-                          />
-                          <p className="legend">Luces posteriores</p>
-                        </div>
-                        <div>
-                          <img
-                            src={api + JSON.parse(event.images).url11}
-                            alt={api + JSON.parse(event.images).url11}
-                          />
-                          <p className="legend">Valvula interna</p>
-                        </div>
-                      </Carousel>
-                    </div>
-                  </div>
-                </div>
+                <ModalImages event={event} />
               </div>
             </div>
           </div>
