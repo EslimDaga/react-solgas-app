a539d4d066ca5fb0dbb9cd5be201c15c91225ec7
Eslim Daga Castro <58963865+EslimDaga@users.noreply.github.com>
2022-01-12 11:18:17 -0500

    Add components, loading data in the table and no results found

---
+++ src/components/common/NoResultsFound.jsx
@@ -0,0 +1,11 @@
+const NoResultsFound = () => {
+  return (
+    <tr>
+      <td colSpan="7" className="text-center p-2">
+        <h2 className="font-bold">No se encontraron resultados. 😢</h2>
+      </td>
+    </tr>
+  );
+}
+
+export default NoResultsFound;
