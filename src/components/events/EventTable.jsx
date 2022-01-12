4d125ff49dad55532f3d6ee6b75c4f8cda852302
Eslim Daga Castro <58963865+EslimDaga@users.noreply.github.com>
2022-01-12 11:19:15 -0500

    Add components, refactoring code.

--- src/components/events/EventTable.jsx
+++ src/components/events/EventTable.jsx
@@ -7,6 +7,8 @@ import { Menu, Transition } from "@headlessui/react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "../../assets/styles/css/events/style.css";
 import Pagination from "../common/Pagination";
+import NoResultsFound from "../common/NoResultsFound";
+import LoadingDataInTable from "../common/LoadingDataInTable";
 
 const EventTable = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -279,52 +281,12 @@ const EventTable = () => {
                                 ))}
                                 {search.length > 0 &&
                                 filteredEvents().length === 0 ? (
-                                  <tr>
-                                    <td colSpan="7" className="text-center p-2">
-                                      <h2 className="font-bold">
-                                        No se encontraron resultados. 😢
-                                      </h2>
-                                    </td>
-                                  </tr>
+                                  <NoResultsFound />
                                 ) : null}
                               </tbody>
                             </table>
                             {isLoading && (
-                              <div className="flex justify-center">
-                                <div className="w-full h-48 bg-gray-100 flex justify-center items-center">
-                                  <div className="w-3/5 bg-green-100 rounded-lg shadow-sm p-5 border-dashed border border-green-500 flex flex-col  justify-between items-center gap-2 sm:gap-0">
-                                    <div className="flex flex-col sm:flex-row justify-start items-center">
-                                      <div className="flex rounded-md">
-                                        <svg
-                                          className="animate-spin -ml-1 mr-3 h-5 w-5 text-blue"
-                                          xmlns="http://www.w3.org/2000/svg"
-                                          fill="none"
-                                          viewBox="0 0 24 24"
-                                        >
-                                          <circle
-                                            className="opacity-25"
-                                            cx="12"
-                                            cy="12"
-                                            r="10"
-                                            stroke="currentColor"
-                                            strokeWidth="4"
-                                          ></circle>
-                                          <path
-                                            className="opacity-75"
-                                            fill="currentColor"
-                                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                                          ></path>
-                                        </svg>
-                                      </div>
-                                      <div className="text-center sm:text-left">
-                                        <h1 className="text-gray-900 font-bold tracking-wider">
-                                          Cargando...
-                                        </h1>
-                                      </div>
-                                    </div>
-                                  </div>
-                                </div>
-                              </div>
+                              <LoadingDataInTable />
                             )}
                           </div>
                         </div>
