e42a46ad81d6b5b54d6156332853b1740caa6b9b
Eslim Daga Castro <58963865+EslimDaga@users.noreply.github.com>
2022-01-13 11:08:48 -0500

    Refactor modal images components

--- src/components/events/EventTable.jsx
+++ src/components/events/EventTable.jsx
@@ -84,29 +84,7 @@ const EventTable = () => {
         <ModalDetails event={event} closeModal={closeModal} />
       ) : null}
       {showModalImages ? (
-        <>
-          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-            <div className="relative w-auto my-6 mx-auto max-w-3xl">
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                <div className="flex items-start justify-between p-3 border-b border-solid border-blueGray-200 rounded-t">
-                  <h3 className="text-1xl font-semibold self-center">
-                    Imagenes del Evento
-                  </h3>
-                  <button
-                    className="p-1 ml-auto bg-transparent border-0 text-gray-900 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                    onClick={closeModalImages}
-                  >
-                    <span className="bg-transparent text-gray-900 h-6 w-6 text-xl block outline-none focus:outline-none">
-                      x
-                    </span>
-                  </button>
-                </div>
-                <ModalImages event={event} />
-              </div>
-            </div>
-          </div>
-          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-        </>
+        <ModalImages event={event} closeModal={closeModalImages} />
       ) : null}
       <div className="bg-white">
         <div className="pt-1">
