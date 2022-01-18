5ff3cfb5b487b37945b6f1fd6efafaa806acf8a2
Eslim Daga Castro <58963865+EslimDaga@users.noreply.github.com>
2022-01-18 14:48:41 -0500

    Fix overflow and better view dropdown

--- src/components/events/EventTable.jsx
+++ src/components/events/EventTable.jsx
@@ -96,7 +96,7 @@ const EventTable = () => {
                     <div className="flex flex-col">
                       <div className="-my-2 overflow-x-auto sm:-mx-3 lg:-mx-8">
                         <div className="py-2 align-middle inline-block min-w-full sm:px-0 lg:px-5">
-                          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                          <div className="sm:rounded-lg">
                             <Table
                               filteredEvents={filteredEvents}
                               openModal={openModal}
