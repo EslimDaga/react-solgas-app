37c7d64a1f48908c0bc0322397380b5dbd231d68
Eslim Daga Castro <58963865+EslimDaga@users.noreply.github.com>
2022-01-18 14:09:50 -0500

    Change size Logo header

--- src/components/common/LogoHeader.jsx
+++ src/components/common/LogoHeader.jsx
@@ -2,8 +2,8 @@ import styled from "styled-components";
 const url = require("../../assets/images/logo-solgas.png");
 
 export const LogoHeader = styled.img`
-  height: 40px;
-  width: 160px;
+  height: 35px;
+  width: 133px;
 `;
 
 LogoHeader.defaultProps = {
