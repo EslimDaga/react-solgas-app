eb216f1a714101f35877ff38b0a7231dc3343c53
Eslim Daga Castro <58963865+EslimDaga@users.noreply.github.com>
2022-01-19 15:07:10 -0500

    Add delete checkpoint in the service checkpoint

--- src/service/checkpoint.js
+++ src/service/checkpoint.js
@@ -14,13 +14,22 @@ export const getCheckpoints = async () => {
 
 export const getCheckpoinByName = async(name) => {
   const token = cache.getItem("user").token;
-  const response = await axios.get(`${api}/control/web/api/get-checkpoint/${name}/`,
-    {
+  const response = await axios.get(`${api}/control/web/api/get-checkpoint/${name}/`,{
       headers: {
         Authorization: `JWT ${token}`,
-      },
+      }
     }
   );
+  return response.data;
+}
+
+export const deleteCheckpoint = async(name) => {
+  const token = cache.getItem("user").token;
+  const response = await axios.delete(`${api}/control/web/api/delete-checkpoint/${name}/`,{
+      headers: {
+        Authorization: `JWT ${token}`,
+      }
+  });
   return response.data;
 }
 
