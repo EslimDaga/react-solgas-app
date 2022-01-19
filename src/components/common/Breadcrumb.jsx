6e1200735d77e10d320a072b70770e3561ee59b2
Eslim Daga Castro <58963865+EslimDaga@users.noreply.github.com>
2022-01-19 10:14:00 -0500

    Update label <a> to <Link>

--- src/components/common/Breadcrumb.jsx
+++ src/components/common/Breadcrumb.jsx
@@ -1,14 +1,16 @@
+import { Link } from "react-router-dom";
+
 const Breadcrumb = ({title}) => {
   return (
     <nav className="flex bg-neutral-50 text-blue-700 py-3 px-7 mb-4 pt-28" aria-label="Breadcrumb">
       <ol className="inline-flex items-center space-x-1 md:space-x-3">
         <li className="inline-flex items-center">
-          <a href="/" className="text-neutral-700 hover:text-blue-900 text-sm inline-flex items-center font-medium">
+          <Link to="/" className="text-neutral-700 hover:text-blue-900 text-sm inline-flex items-center font-medium">
             <svg className="w-5 h-5 mr-2.5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
               <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
             </svg>
             Inicio
-          </a>
+          </Link>
         </li>
         <li>
           <div className="flex items-center">
