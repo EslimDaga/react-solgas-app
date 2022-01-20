666b9fc0d3475f0cedf854226e48a9ec6bd91975
Eslim Daga Castro <58963865+EslimDaga@users.noreply.github.com>
2022-01-20 10:09:33 -0500

    Refactor checkpoint table

--- src/components/checkpoint/CheckpointTable.jsx
+++ src/components/checkpoint/CheckpointTable.jsx
@@ -7,6 +7,9 @@ import cache from "../../helpers/cache";
 import InputSearch from "../common/InputSearch";
 import Table from "./Table";
 import LoadingDataInTable from "../common/LoadingDataInTable";
+import ModalCreateCheckpoint from "./ModalCreateCheckpoint";
+import ModalViewCheckpoint from "./ModalViewCheckpoint";
+import Pagination from "../common/Pagination";
 
 render(
   <Global
@@ -87,86 +90,17 @@ const CheckpointTable = () => {
   return (
     <>
       {showModalCreateCheckpoint && (
-        <>
-          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-            <div className="">
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none h-100-vh">
-                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
-                  <h3 className="text-1xl font-semibold self-center">
-                    Detalles del Evento
-                  </h3>
-                  <button
-                    className="p-1 ml-auto bg-transparent border-0 text-gray-900 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                    onClick={closeModalCreateCheckpoint}
-                  >
-                    <span className="bg-transparent text-gray-900 h-6 w-6 text-xl block outline-none focus:outline-none">
-                      x
-                    </span>
-                  </button>
-                </div>
-                <div className="relative">
-                  <div className="w-full bg-gray-100">
-                    <div className="bg-white rounded-lg shadow-sm">
-                      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1">
-                        <div className="flex flex-col sm:mt-0 gap-7 text-sm">
-                          <iframe
-                            id="inlineFrameExample"
-                            title="Inline Frame Example"
-                            width="100%"
-                            height="770px"
-                            src={`http://checkpoint.segursat.com/api/create-checkpoint/${token}`}
-                          ></iframe>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-        </>
+        <ModalCreateCheckpoint
+          closeModalCreateCheckpoint={closeModalCreateCheckpoint}
+          token={token}
+        />
       )}
       {showModalViewCheckpoint && (
-        <>
-          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-            <div className="">
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none h-100-vh">
-                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
-                  <h3 className="text-1xl font-semibold self-center">
-                    Detalles del Evento
-                  </h3>
-                  <button
-                    className="p-1 ml-auto bg-transparent border-0 text-gray-900 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                    onClick={closeModalViewCheckpoint}
-                  >
-                    <span className="bg-transparent text-gray-900 h-6 w-6 text-xl block outline-none focus:outline-none">
-                      x
-                    </span>
-                  </button>
-                </div>
-                <div className="relative">
-                  <div className="w-full bg-gray-100">
-                    <div className="bg-white rounded-lg shadow-sm">
-                      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1">
-                        <div className="flex flex-col sm:mt-0 gap-7 text-sm">
-                          <iframe
-                            id="inlineFrameExample"
-                            title="Inline Frame Example"
-                            width="100%"
-                            height="698px"
-                            src={`http://checkpoint.segursat.com/api/get-checkpoint/${checkpoint.name}/${token}`}
-                          ></iframe>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-        </>
+        <ModalViewCheckpoint
+          closeModalViewCheckpoint={closeModalViewCheckpoint}
+          checkpoint={checkpoint}
+          token={token}
+        />
       )}
       <div className="bg-white">
         <div className="pt-1">
@@ -195,10 +129,10 @@ const CheckpointTable = () => {
                           <div className="sm:rounded-lg">
                             <Table
                               filteredCheckpoints={filteredCheckpoints}
-                              openModalViewCheckpoin={openModalViewCheckpoint}
+                              openModalViewCheckpoint={openModalViewCheckpoint}
                               search={search}
                             />
-                            {isLoading && (<LoadingDataInTable />)}
+                            {isLoading && <LoadingDataInTable />}
                           </div>
                         </div>
                       </div>
@@ -206,20 +140,7 @@ const CheckpointTable = () => {
                   </div>
                 </div>
               </div>
-              <div className="mb-3">
-                <button
-                  className="bg-blue-900 text-white font-bold py-2 px-4 rounded mr-2"
-                  onClick={prevPage}
-                >
-                  Anterior
-                </button>
-                <button
-                  className="bg-blue-900 text-white font-bold py-2 px-4 rounded"
-                  onClick={nextPage}
-                >
-                  Siguiente
-                </button>
-              </div>
+              <Pagination prevPage={prevPage} nextPage={nextPage}/>
             </section>
           </div>
         </div>
