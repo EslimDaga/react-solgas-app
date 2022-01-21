268ac5e5dede7b149647b880d6728291c429607c
Eslim Daga Castro <58963865+EslimDaga@users.noreply.github.com>
2022-01-21 10:10:05 -0500

    Change Link to url

--- src/components/common/Breadcrumb.jsx
+++ src/components/common/Breadcrumb.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-const Breadcrumb = ({title}) => {
+const Breadcrumb = ({ title, url }) => {
   return (
     <nav className="flex bg-neutral-50 text-blue-700 py-3 px-7 mb-4 pt-28" aria-label="Breadcrumb">
       <ol className="inline-flex items-center space-x-1 md:space-x-3">
@@ -17,7 +17,7 @@ const Breadcrumb = ({title}) => {
             <svg className="w-6 h-6 text-blue-900" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
               <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path>
             </svg>
-            <a href="/" className="text-blue-900 hover:text-blue-900 ml-1 md:ml-2 text-sm font-bold">{title}</a>
+            <Link to={url} className="text-blue-900 hover:text-blue-900 ml-1 md:ml-2 text-sm font-bold">{title}</Link>
           </div>
         </li>
       </ol>
