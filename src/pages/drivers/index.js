cf9f224e8b54107fd84f98193b61b94395f9e111
Eslim Daga Castro <58963865+EslimDaga@users.noreply.github.com>
2022-01-27 11:32:01 -0500

    Add header and breadcum in driver page

--- src/pages/drivers/index.js
+++ src/pages/drivers/index.js
@@ -1,5 +1,13 @@
+import Breadcrumb from "../../components/common/Breadcrumb";
+import Header from "../../components/Header";
+
 const DriverPage = () => {
-  return <div>Drivers</div>;
+  return (
+    <>
+      <Header />
+      <Breadcrumb title="Conductores" />
+    </>
+  );
 };
 
 export default DriverPage;
