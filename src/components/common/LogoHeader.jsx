4844a2a5f6d22d5d4dd2a01b79bb922d7ea680a7
Eslim Daga Castro <58963865+EslimDaga@users.noreply.github.com>
2022-02-01 17:33:45 -0500

    Add dark mode and white mode in responsive header

--- src/components/common/LogoHeader.jsx
+++ src/components/common/LogoHeader.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 const url = require("../../assets/images/logo-solgas.png");
+const url_dark = require("../../assets/images/logo-solgas-dark.png");
 
 export const LogoHeader = styled.img`
   height: 35px;
@@ -8,4 +9,13 @@ export const LogoHeader = styled.img`
 
 LogoHeader.defaultProps = {
   src: url
+}
+
+export const LogoHeaderDark = styled.img`
+  height: 35px;
+  width: 133px;
+`;
+
+LogoHeaderDark.defaultProps = {
+  src: url_dark
 }
