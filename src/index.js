bc4f2c052d9a387de30f1ad66da14d340e8d3930
Eslim Daga Castro <58963865+EslimDaga@users.noreply.github.com>
2022-02-01 15:58:35 -0500

    Change quotation marks

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { ThemeProvider } from "./store/context/ThemeContext";
 
 ReactDOM.render(
-  <App />,
-document.getElementById('root'));
+  <ThemeProvider>
+    <App />
+  </ThemeProvider>,
+document.getElementById("root"));
