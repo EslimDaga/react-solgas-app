243c12ebbc38cd28940596410c5ff73330fca4a9
Eslim Daga Castro <58963865+EslimDaga@users.noreply.github.com>
2022-02-02 10:16:56 -0500

    Add dark mode in page events

--- src/components/common/ModalImages.jsx
+++ src/components/common/ModalImages.jsx
@@ -6,23 +6,23 @@ const ModalImages = ({event, closeModalImages}) => {
     <>
       <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
         <div className="relative w-auto my-6 mx-auto max-w-3xl">
-          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-            <div className="flex items-start justify-between p-3 border-b border-solid border-blueGray-200 rounded-t">
-              <h3 className="text-1xl font-semibold self-center">
+          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white dark:bg-gray-700 outline-none focus:outline-none">
+            <div className="flex items-start justify-between p-3 border-solid border-blueGray-200 rounded-t">
+              <h3 className="text-1xl font-semibold self-center dark:text-gray-100">
                 Imagenes del Evento
               </h3>
               <button
-                className="p-1 ml-auto bg-transparent border-0 text-gray-900 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                className="p-1 ml-auto bg-transparent border-0 text-gray-900 dark:text-gray-100 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                 onClick={closeModalImages}
               >
-                <span className="bg-transparent text-gray-900 h-6 w-6 text-xl block outline-none focus:outline-none">
+                <span className="bg-transparent text-gray-900 dark:text-gray-100 h-6 w-6 text-xl block outline-none focus:outline-none">
                   x
                 </span>
               </button>
             </div>
             <div className="relative flex-auto">
-              <div className="w-full bg-gray-100 flex">
-                <div className="bg-white rounded-lg shadow-sm">
+              <div className="w-full bg-gray-100 dark:bg-gray-700 rounded-lg flex">
+                <div className="bg-white dark:bg-gray-700 rounded-lg shadow-sm">
                   <Carousel
                     autoPlay
                     width={500}
