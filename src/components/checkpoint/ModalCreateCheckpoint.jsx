f570c864337bb01c033edf6e9d828ba5ccd678e8
Eslim Daga Castro <58963865+EslimDaga@users.noreply.github.com>
2022-02-03 12:46:00 -0500

    Add dark mode in background and text

--- src/components/checkpoint/ModalCreateCheckpoint.jsx
+++ src/components/checkpoint/ModalCreateCheckpoint.jsx
@@ -3,16 +3,16 @@ const ModalCreateCheckpoint = ({ closeModalCreateCheckpoint, token }) => {
     <>
       <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
         <div className="">
-          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none h-100-vh">
+          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white dark:bg-gray-700 outline-none focus:outline-none h-100-vh">
             <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
-              <h3 className="text-1xl font-semibold self-center">
-                Detalles del Evento
+              <h3 className="dark:text-gray-100 text-1xl font-semibold self-center">
+                Crear Checkpoint
               </h3>
               <button
                 className="p-1 ml-auto bg-transparent border-0 text-gray-900 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                 onClick={closeModalCreateCheckpoint}
               >
-                <span className="bg-transparent text-gray-900 h-6 w-6 text-xl block outline-none focus:outline-none">
+                <span className="bg-transparent text-gray-900 dark:text-gray-100 h-6 w-6 text-xl block outline-none focus:outline-none">
                   x
                 </span>
               </button>
