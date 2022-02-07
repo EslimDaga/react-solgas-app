60a02a3f1994afb8314ce5da122bcfc0c03dc2ca
Eslim Daga Castro <58963865+EslimDaga@users.noreply.github.com>
2022-02-07 15:24:01 -0500

    Add driver table component

--- src/components/drivers/DriverTable.jsx
+++ src/components/drivers/DriverTable.jsx
@@ -1,7 +1,46 @@
+import Pagination from "../common/Pagination";
+import InputSearch from "../common/InputSearch";
+import Table from "./Table";
+import { PlusCircleIcon } from "@heroicons/react/solid";
+
 const DriverTable = () => {
   return (
-    <h1>Driver</h1>
-  )
+    <div className="bg-white dark:bg-gray-800">
+      <div className="pt-1">
+        <div className="mx-auto px-4 flex items-center space-x-2 sm:px-6 lg:max-w-full lg:px-6">
+          <section className="antialiased text-gray-600 w-full">
+            <div className="flex flex-col justify-center">
+              <div className="items-center pb-3 flex sm:flex lg:flex justify-between">
+                <InputSearch
+                  label="Buscar por Dni"
+                />
+                <button
+                  className="sm:ml-0 sm:mt-1 lg:ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-bold text-white bg-blue-900"
+                >
+                  <PlusCircleIcon className="w-5 h-5 mr-2" />
+                  Agregar Conductor
+                </button>
+              </div>
+              <div className="w-full mx-auto bg-white rounded-lg">
+                <div className="lg:pr-3 sm:pr-1 lg:pl-3 sm:pl-1 pb-3 dark:bg-gray-800">
+                  <div className="flex flex-col">
+                    <div className="-my-2 overflow-x-auto sm:-mx-3 lg:-mx-8">
+                      <div className="py-2 align-middle inline-block min-w-full sm:px-0 lg:px-5">
+                        <div className="sm:rounded-lg">
+                          <Table />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <Pagination />
+          </section>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DriverTable;
