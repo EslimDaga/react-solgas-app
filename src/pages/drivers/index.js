e5e4d19cb7e5218957d7df9cba153d91d915fab1
Eslim Daga Castro <58963865+EslimDaga@users.noreply.github.com>
2022-02-08 18:01:02 -0500

    Add provider - Hooks

--- src/pages/drivers/index.js
+++ src/pages/drivers/index.js
@@ -3,18 +3,22 @@ import Header from "../../components/Header";
 import { Helmet } from "react-helmet";
 import Favicon from "../../assets/images/ico-solgas.png";
 import DriverTable from "../../components/drivers/DriverTable";
+import { Provider } from "react-redux";
+import store from "../../store/store";
 
 const DriverPage = () => {
   return (
-    <section className="h-screen bg-white dark:bg-gray-800">
-      <Helmet>
-        <title>Solgas - Conductores</title>
-        <link rel="icon" type="image/png" href={Favicon} sizes="16x16" />
-      </Helmet>
-      <Header />
-      <Breadcrumb title="Conductores" url="/drivers" />
-      <DriverTable />
-    </section>
+    <Provider store={store}>
+      <section className="h-screen bg-white dark:bg-gray-800">
+        <Helmet>
+          <title>Solgas - Conductores</title>
+          <link rel="icon" type="image/png" href={Favicon} sizes="16x16" />
+        </Helmet>
+        <Header />
+        <Breadcrumb title="Conductores" url="/drivers" />
+        <DriverTable />
+      </section>
+    </Provider>
   );
 };
 
