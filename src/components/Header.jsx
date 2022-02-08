8a1e1bfeee6732f99d49b3837c75d6a7e56eaee4
Eslim Daga Castro <58963865+EslimDaga@users.noreply.github.com>
2022-02-08 18:00:31 -0500

    Fix links hover

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,10 +12,11 @@ render(
   <Global
     styles={css`
       .active {
-        background-color: #1E3A8A;
-        color: #FFF;
-      }&.active:hover {
-        color: #FFF;
+        background-color: #cacdd7;
+        color: #fff;
+      }
+      &.active:hover {
+        color: #fff;
       }
     `}
   />
@@ -57,7 +58,7 @@ const Header = () => {
                   key={item.path}
                   end
                   to={`/${item.path}`}
-                  className="text-base font-medium text-gray-500 dark:text-gray-100 hover:text-gray-900 px-2 py-2 rounded-md"
+                  className="text-base font-medium text-gray-400 dark:text-gray-500 hover:text-gray-900 px-2 py-2 rounded-md"
                 >
                   {item.title}
                 </NavLink>
