e533fbfb26b9b16bff81f0c39b95c54e479e5246
Eslim Daga Castro <58963865+EslimDaga@users.noreply.github.com>
2022-02-09 10:40:52 -0500

    Fix size modals

--- src/components/checkpoint/ModalCreateCheckpoint.jsx
+++ src/components/checkpoint/ModalCreateCheckpoint.jsx
@@ -3,7 +3,7 @@ const ModalCreateCheckpoint = ({ closeModalCreateCheckpoint, token }) => {
     <>
       <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
         <div className="">
-          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white dark:bg-gray-700 outline-none focus:outline-none h-100-vh">
+          <div className="border-0 rounded-lg shadow-lg relative flex flex-col bg-white dark:bg-gray-700 outline-none focus:outline-none h-100-vh">
             <div className="flex items-start justify-between p-5 border-blueGray-200 rounded-t">
               <h3 className="dark:text-gray-100 text-1xl font-semibold self-center">
                 Crear Checkpoint
