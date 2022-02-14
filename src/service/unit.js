383e705f853ba76ab2b941b2b00f75fa5163d18f
Eslim Daga Castro <58963865+EslimDaga@users.noreply.github.com>
2022-02-14 16:20:11 -0500

    Add service create and read all units

---
+++ src/service/unit.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import cache from "../helpers/cache";
+import { api } from "../constants/global";
+
+export const getUnits = async () => {
+  const token = cache.getItem("user").token;
+  const response = await axios.get(`${api}/control/web/api/get-units/`, {
+    headers: {
+      Authorization: `JWT ${token}`,
+    },
+  });
+  return response;
+}
+
+export const createUnit = async (data) => {
+  const token = cache.getItem("user").token;
+  const response = await axios.post(`${api}/control/web/api/create-unit/`, data, {
+    headers: {
+      "Authorization": `JWT ${token}`
+    }
+  });
+  return response;
+}
