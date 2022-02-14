00f00b0c92e773bc007b167791a4009a10a6d371
Eslim Daga Castro <58963865+EslimDaga@users.noreply.github.com>
2022-02-14 16:19:59 -0500

    Add provider in the units view

--- src/pages/units/index.js
+++ src/pages/units/index.js
@@ -1,15 +1,19 @@
 import Header from "../../components/Header";
 import Breadcrumb from "../../components/common/Breadcrumb";
 import UnitTable from "../../components/units/UnitTable";
+import { Provider } from "react-redux";
+import store from "../../store/store";
 
 const UnitPage = () => {
   return (
-    <>
-      <Header />
-      <Breadcrumb title="Unidades" url="/units" />
-      <UnitTable />
-    </>
-  )
+    <Provider store={store}>
+      <section className="h-screen bg-white dark:bg-gray-800">
+        <Header />
+        <Breadcrumb title="Unidades" url="/units" />
+        <UnitTable />
+      </section>
+    </Provider>
+  );
 }
 
 export default UnitPage;
