4a53fefa4f743c9399676843181a862620a20efd
Eslim Daga Castro <58963865+EslimDaga@users.noreply.github.com>
2022-02-14 16:20:37 -0500

    Add retrieve units action

---
+++ src/store/actions/Units.js
@@ -0,0 +1,17 @@
+import { GET_UNIT, CREATE_UNIT, DELETE_UNIT, SHOW_LOADING } from "./UnitTypes";
+import { getUnits } from "../../service/unit";
+
+export const retrieveUnits = () => async (dispatch) => {
+  dispatch({
+    type: SHOW_LOADING,
+  })
+  try{
+    const res = await getUnits();
+    dispatch({
+      type: GET_UNIT,
+      payload: res.data
+    });
+  }catch(err){
+    console.log(err);
+  }
+};
