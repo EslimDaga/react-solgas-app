8a6413bd70019be90999db62e4a7798da01ddf7e
Eslim Daga Castro <58963865+EslimDaga@users.noreply.github.com>
2022-02-14 11:58:32 -0500

    Add unit page

---
+++ src/pages/units/index.js
@@ -0,0 +1,14 @@
+import Header from "../../components/Header";
+import Breadcrumb from "../../components/common/Breadcrumb";
+
+const UnitPage = () => {
+  return (
+    <>
+      <Header />
+      <Breadcrumb title="Unidades" url="/units" />
+      <div>index</div>
+    </>
+  )
+}
+
+export default UnitPage;
