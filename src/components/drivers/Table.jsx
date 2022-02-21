c25892cc4847f95d388a033308d43486db697e07
Eslim Daga Castro <58963865+EslimDaga@users.noreply.github.com>
2022-02-21 09:56:56 -0500

    Add table content

--- src/components/drivers/Table.jsx
+++ src/components/drivers/Table.jsx
@@ -1,7 +1,45 @@
 const Table = () => {
   return (
-    <div>Table</div>
-  )
+    <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+      <thead className="bg-blue-900 dark:bg-gray-900">
+        <tr>
+          <th
+            scope="col"
+            className="px-6 py-3 text-left text-sm font-bold text-white uppercase tracking-wider"
+          >
+            Dni
+          </th>
+          <th
+            scope="col"
+            className="px-6 py-3 text-left text-sm font-bold text-white uppercase tracking-wider"
+          >
+            Apellidos
+          </th>
+          <th
+            scope="col"
+            className="px-6 py-3 text-left text-sm font-bold text-white uppercase tracking-wider"
+          >
+            Nombres
+          </th>
+          <th
+            scope="col"
+            className="px-6 py-3 text-left text-sm font-bold text-white uppercase tracking-wider"
+          >
+            Número de Licencia
+          </th>
+          <th
+            scope="col"
+            className="px-6 py-3 text-left text-sm font-bold text-white uppercase tracking-wider"
+          >
+            Acciones
+          </th>
+        </tr>
+      </thead>
+      <tbody className="bg-white dark:bg-gray-700 divide-y divide-gray-200 dark:divide-gray-600">
+
+      </tbody>
+    </table>
+  );
 }
 
 export default Table
