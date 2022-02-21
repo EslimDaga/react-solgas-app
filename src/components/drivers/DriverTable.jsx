a2c765b9b9ac963ee70bae7cabca5c985ec2a9a8
Eslim Daga Castro <58963865+EslimDaga@users.noreply.github.com>
2022-02-21 10:29:37 -0500

    Add axios get drivers

--- src/components/drivers/DriverTable.jsx
+++ src/components/drivers/DriverTable.jsx
@@ -1,9 +1,62 @@
+import { useState, useEffect } from "react";
 import InputSearch from "../common/InputSearch";
 import Table from "./Table";
 import Pagination from "../common/Pagination";
+import { getDrivers } from "../../service/driver";
 import { PlusCircleIcon } from "@heroicons/react/solid";
 
 const DriverTable = () => {
+
+  const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState("");
+  const [currentPage, setCurrentPage] = useState(0);
+  const [drivers, setDrivers] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+    getDrivers().then(response => {
+      setLoading(false);
+      setDrivers(response.data);
+    })
+  },[])
+
+  const filteredDrivers = () => {
+    if(search.length === 0){
+      return drivers.slice(currentPage, currentPage + 10);
+    }
+    const filtered = drivers.filter((driver) => {
+      return (
+        driver.lastname.toLowerCase().includes(search.toLowerCase()) ||
+        driver.firstname.toLowerCase().includes(search.toLowerCase()) ||
+        driver.license_number.toLowerCase().includes(search.toLowerCase())
+      );
+    });
+    return filtered.slice(currentPage, currentPage + 10);
+  }
+
+  const onSearchChange = ({ target }) => {
+    setCurrentPage(0);
+    setSearch(target.value);
+  };
+
+  const nextPage = () => {
+    if(drivers.filter((driver) => {
+      return (
+        driver.lastname.toLowerCase().includes(search.toLowerCase()) ||
+        driver.firstname.toLowerCase().includes(search.toLowerCase()) ||
+        driver.license_number.toLowerCase().includes(search.toLowerCase())
+      );
+    }).length > currentPage + 10){
+      setCurrentPage(currentPage + 10);
+    }
+  }
+
+  const prevPage = () => {
+    if(currentPage > 0){
+      setCurrentPage(currentPage - 10);
+    }
+  }
+
   return (
     <>
       <div className="bg-white dark:bg-gray-800">
@@ -14,10 +67,10 @@ const DriverTable = () => {
                 <div className="items-center pb-3 flex sm:flex lg:flex justify-between">
                   <InputSearch
                     label="Buscar"
+                    search={search}
+                    onSearchChange={onSearchChange}
                   />
-                  <button
-                    className="sm:ml-0 sm:mt-1 lg:ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-bold text-white bg-blue-900"
-                  >
+                  <button className="sm:ml-0 sm:mt-1 lg:ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-bold text-white bg-blue-900">
                     <PlusCircleIcon className="w-5 h-5 mr-2" />
                     Agregar Conductor
                   </button>
@@ -29,6 +82,9 @@ const DriverTable = () => {
                         <div className="py-2 align-middle inline-block min-w-full sm:px-0 lg:px-5">
                           <div className="sm:rounded-lg">
                             <Table
+                              filteredDrivers={filteredDrivers}
+                              search={search}
+                              loading={loading}
                             />
                           </div>
                         </div>
@@ -37,13 +93,13 @@ const DriverTable = () => {
                   </div>
                 </div>
               </div>
-              <Pagination />
+              <Pagination nextPage={nextPage} prevPage={prevPage} />
             </section>
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
 
 export default DriverTable
