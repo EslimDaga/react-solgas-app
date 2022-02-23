8c31cd6d0ffc198501f534a726f1cb84c7cd1911
Eslim Daga Castro <58963865+EslimDaga@users.noreply.github.com>
2022-02-23 11:00:04 -0500

    Fix unit modal create responsive

--- src/components/units/UnitTable.jsx
+++ src/components/units/UnitTable.jsx
@@ -83,8 +83,8 @@ const UnitTable = () => {
       {showModalCreateUnit && (
         <>
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-            <div className="">
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col bg-white dark:bg-gray-700 outline-none focus:outline-none h-100-vh">
+            <div className="relative w-full my-6 mx-4 max-w-3xl">
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col bg-white dark:bg-gray-700 outline-none focus:outline-none">
                 <div className="flex items-start justify-between p-5 border-blueGray-200 rounded-t">
                   <h3 className="dark:text-gray-100 text-1xl font-semibold self-center">
                     Crear Unidad
@@ -324,4 +324,4 @@ const UnitTable = () => {
   );
 }
 
-export default UnitTable
+export default UnitTable;
