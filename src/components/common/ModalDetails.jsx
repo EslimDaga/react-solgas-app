0cb629de5398f425e9fef66dabde4b2e477474a7
Eslim Daga Castro <58963865+EslimDaga@users.noreply.github.com>
2022-02-23 16:12:52 -0500

    Fix modal details events

--- src/components/common/ModalDetails.jsx
+++ src/components/common/ModalDetails.jsx
@@ -22,7 +22,7 @@ const ModalDetails = ({ event, closeModal }) => {
             <div className="relative p-5 flex-auto">
               <div className="w-full bg-gray-100 dark:bg-gray-800 flex">
                 <div className="bg-white dark:bg-gray-800 rounded-lg shadow-sm">
-                  <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-10">
+                  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-2">
                     <div className="flex flex-col sm:mt-0 gap-7 text-sm">
                       <div className="bg-blue-900 flex justify-between items-center p-3 rounded-md shadow-sm">
                         <div className="flex justify-start items-center gap-2">
