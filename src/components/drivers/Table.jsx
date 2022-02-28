65036ddcd170d6ad678175fb466a09d693243a14
Eslim Daga Castro <58963865+EslimDaga@users.noreply.github.com>
2022-02-28 15:29:05 -0500

    Change color buttons

--- src/components/drivers/Table.jsx
+++ src/components/drivers/Table.jsx
@@ -1,6 +1,6 @@
 import { Fragment } from "react";
 import { Menu, Transition } from "@headlessui/react";
-import { ChevronDownIcon } from "@heroicons/react/solid";
+import { ChevronDownIcon, TrashIcon } from "@heroicons/react/solid";
 import NoResultsFound from "../common/NoResultsFound";
 import LoadingDataInTable from "../common/LoadingDataInTable";
 
@@ -66,36 +66,17 @@ const Table = ({ filteredDrivers, handleDelete, search, loading }) => {
               <td className="px-6 py-4 whitespace-nowrap text-sm font-bold text-gray-500 dark:text-gray-100">
                 <Menu as="div" className="relative inline-block text-left">
                   <div>
-                    <Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300 dark:border-gray-800 shadow-sm px-4 py-2 bg-white dark:bg-gray-800 text-sm font-medium text-gray-700 dark:text-gray-100 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 dark:focus:ring-offset-gray-800 focus:ring-blue-900">
-                      Acción
-                      <ChevronDownIcon
-                        className="-mr-1 ml-2 h-5 w-5"
+                    <button
+                      className="inline-flex justify-center w-full rounded-md border border-gray-300 dark:border-gray-800 shadow-sm px-4 py-2 bg-red-600 dark:bg-red-600 text-sm font-bold text-gray-100 dark:text-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 dark:focus:ring-offset-gray-800 focus:ring-red-900"
+                      onClick={() => handleDelete(driver.dni)}
+                    >
+                      Eliminar
+                      <TrashIcon
+                        className="-mr-1 ml-1 h-5 w-5"
                         aria-hidden="true"
                       />
-                    </Menu.Button>
+                    </button>
                   </div>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-50 rounded-md shadow-lg bg-white dark:bg-gray-800 ring-1 ring-black ring-opacity-5 focus:outline-none z-50">
-                      <div className="py-1 bg-gray-100 dark:bg-gray-800">
-                        <Menu.Item>
-                          <button
-                            className="bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-gray-100 w-full block px-4 py-2 text-sm"
-                            onClick={() => handleDelete(driver.dni)}
-                          >
-                            Eliminar
-                          </button>
-                        </Menu.Item>
-                      </div>
-                    </Menu.Items>
-                  </Transition>
                 </Menu>
               </td>
             </tr>
--- src/components/units/Table.jsx
+++ src/components/units/Table.jsx
@@ -1,5 +1,5 @@
 import { Menu, Transition } from "@headlessui/react";
-import { ChevronDownIcon } from "@heroicons/react/solid";
+import { ChevronDownIcon, TrashIcon } from "@heroicons/react/solid";
 import { Fragment } from "react";
 import LoadingDataInTable from "../common/LoadingDataInTable";
 import NoResultsFound from "../common/NoResultsFound";
@@ -84,36 +84,17 @@ const Table = ({ filteredUnits, search, loading, handleDeleteUnit }) => {
               <td className="px-6 py-4 whitespace-nowrap text-sm font-bold text-gray-500 dark:text-gray-100">
                 <Menu as="div" className="relative inline-block text-left">
                   <div>
-                    <Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300 dark:border-gray-800 shadow-sm px-4 py-2 bg-white dark:bg-gray-800 text-sm font-medium text-gray-700 dark:text-gray-100 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 dark:focus:ring-offset-gray-800 focus:ring-blue-900">
-                      Acción
-                      <ChevronDownIcon
-                        className="-mr-1 ml-2 h-5 w-5"
+                    <button
+                      className="inline-flex justify-center w-full rounded-md border border-gray-300 dark:border-gray-800 shadow-sm px-4 py-2 bg-red-600 dark:bg-red-600 text-sm font-bold text-gray-100 dark:text-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 dark:focus:ring-offset-gray-800 focus:ring-red-900"
+                      onClick={() => handleDeleteUnit(unit.license_plate)}
+                    >
+                      Eliminar
+                      <TrashIcon
+                        className="-mr-1 ml-1 h-5 w-5"
                         aria-hidden="true"
                       />
-                    </Menu.Button>
+                    </button>
                   </div>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-50 rounded-md shadow-lg bg-white dark:bg-gray-800 ring-1 ring-black ring-opacity-5 focus:outline-none z-50">
-                      <div className="py-1 bg-gray-100 dark:bg-gray-800">
-                        <Menu.Item>
-                          <button
-                            className="bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-gray-100 w-full block px-4 py-2 text-sm"
-                            onClick={() => handleDeleteUnit(unit.license_plate)}
-                          >
-                            Eliminar
-                          </button>
-                        </Menu.Item>
-                      </div>
-                    </Menu.Items>
-                  </Transition>
                 </Menu>
               </td>
             </tr>
