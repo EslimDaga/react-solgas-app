f5a79ed4b5d7a0e1a4ba6b14086b6c3d05929a3d
Eslim Daga Castro <58963865+EslimDaga@users.noreply.github.com>
2022-02-28 15:08:26 -0500

    Change color to gray 800

--- src/components/common/ModalImages.jsx
+++ src/components/common/ModalImages.jsx
@@ -40,8 +40,8 @@ const ModalImages = ({event, closeModalImages}) => {
     <>
       <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
         <div className="relative w-full my-6 mx-auto max-w-lg">
-          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white dark:bg-gray-700 outline-none focus:outline-none">
-            <div className="flex items-start justify-between p-3 border-solid border-blueGray-200 rounded-t">
+          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white dark:bg-gray-800 outline-none focus:outline-none">
+            <div className="flex items-start justify-between p-3 border-solid border-gray-700 rounded-t">
               <h3 className="text-1xl font-semibold self-center dark:text-gray-100">
                 Imagenes del Evento
               </h3>
@@ -55,8 +55,8 @@ const ModalImages = ({event, closeModalImages}) => {
               </button>
             </div>
             <div className="">
-              <div className=" bg-gray-100 dark:bg-gray-700 rounded-lg">
-                <div className="bg-white dark:bg-gray-700 rounded-lg shadow-sm">
+              <div className=" bg-gray-100 dark:bg-gray-800 rounded-lg">
+                <div className="bg-white dark:bg-gray-800 rounded-lg shadow-sm">
                   {event.type_of_service === "ENVASADO" ? (
                     <Carousel dynamicHeight={20} thumbWidth={35}>
                       <div>
--- src/components/drivers/DriverTable.jsx
+++ src/components/drivers/DriverTable.jsx
@@ -210,7 +210,7 @@ const DriverTable = () => {
         <>
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
             <div className="relative w-full my-6 mx-4 max-w-3xl">
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col bg-white dark:bg-gray-700 outline-none focus:outline-none">
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col bg-white dark:bg-gray-800 outline-none focus:outline-none">
                 <div className="flex items-start justify-between p-5 border-blueGray-200 rounded-t">
                   <h3 className="dark:text-gray-100 text-1xl font-semibold self-center">
                     Crear Conductores
