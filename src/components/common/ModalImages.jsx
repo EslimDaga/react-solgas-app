410df61fa4d6cbfa1fa731a101cc24cd0513fedc
Eslim Daga Castro <58963865+EslimDaga@users.noreply.github.com>
2022-02-28 12:05:14 -0500

    Fix modal images envasado and other

--- src/components/common/ModalImages.jsx
+++ src/components/common/ModalImages.jsx
@@ -57,85 +57,174 @@ const ModalImages = ({event, closeModalImages}) => {
             <div className="">
               <div className=" bg-gray-100 dark:bg-gray-700 rounded-lg">
                 <div className="bg-white dark:bg-gray-700 rounded-lg shadow-sm">
-                  <Carousel dynamicHeight={20} thumbWidth={35}>
-                    <div>
-                      <img
-                        src={api + JSON.parse(event.images).url1}
-                        alt={api + JSON.parse(event.images).url1}
-                      />
-                      <p className="legend">Selfie del Conductor</p>
-                    </div>
-                    <div>
-                      <img
-                        src={api + JSON.parse(event.images).url2}
-                        alt={api + JSON.parse(event.images).url2}
-                      />
-                      <p className="legend">Extintor</p>
-                    </div>
-                    <div>
-                      <img
-                        src={api + JSON.parse(event.images).url3}
-                        alt={api + JSON.parse(event.images).url3}
-                      />
-                      <p className="legend">Delantero Izquierdo</p>
-                    </div>
-                    <div>
-                      <img
-                        src={api + JSON.parse(event.images).url4}
-                        alt={api + JSON.parse(event.images).url4}
-                      />
-                      <p className="legend">Delantero Derecho</p>
-                    </div>
-                    <div>
-                      <img
-                        src={api + JSON.parse(event.images).url5}
-                        alt={api + JSON.parse(event.images).url5}
-                      />
-                      <p className="legend">Posterior Izquiera</p>
-                    </div>
-                    <div>
-                      <img
-                        src={api + JSON.parse(event.images).url6}
-                        alt={api + JSON.parse(event.images).url6}
-                      />
-                      <p className="legend">Posterior Derecha</p>
-                    </div>
-                    <div>
-                      <img
-                        src={api + JSON.parse(event.images).url7}
-                        alt={api + JSON.parse(event.images).url7}
-                      />
-                      <p className="legend">Toma frontal de la unidad</p>
-                    </div>
-                    <div>
-                      <img
-                        src={api + JSON.parse(event.images).url8}
-                        alt={api + JSON.parse(event.images).url8}
-                      />
-                      <p className="legend">Toma posterior de la unidad</p>
-                    </div>
-                    <div>
-                      <img
-                        src={api + JSON.parse(event.images).url9}
-                        alt={api + JSON.parse(event.images).url9}
-                      />
-                      <p className="legend">Luces delanteras</p>
-                    </div>
-                    <div>
-                      <img
-                        src={api + JSON.parse(event.images).url10}
-                        alt={api + JSON.parse(event.images).url10}
-                      />
-                      <p className="legend">Luces posteriores</p>
-                    </div>
-                    <div>
-                      <img
-                        src={api + JSON.parse(event.images).url11}
-                        alt={api + JSON.parse(event.images).url11}
-                      />
-                      <p className="legend">Valvula interna</p>
-                    </div>
-                  </Carousel>
+                  {event.type_of_service === "ENVASADO" ? (
+                    <Carousel dynamicHeight={20} thumbWidth={35}>
+                      <div>
+                        <img
+                          src={api + JSON.parse(event.images).url1}
+                          alt={api + JSON.parse(event.images).url1}
+                        />
+                        <p className="legend">Selfie del Conductor</p>
+                      </div>
+                      <div>
+                        <img
+                          src={api + JSON.parse(event.images).url2}
+                          alt={api + JSON.parse(event.images).url2}
+                        />
+                        <p className="legend">Extintor</p>
+                      </div>
+                      <div>
+                        <img
+                          src={api + JSON.parse(event.images).url3}
+                          alt={api + JSON.parse(event.images).url3}
+                        />
+                        <p className="legend">Delantero Izquierdo</p>
+                      </div>
+                      <div>
+                        <img
+                          src={api + JSON.parse(event.images).url4}
+                          alt={api + JSON.parse(event.images).url4}
+                        />
+                        <p className="legend">Delantero Derecho</p>
+                      </div>
+                      <div>
+                        <img
+                          src={api + JSON.parse(event.images).url5}
+                          alt={api + JSON.parse(event.images).url5}
+                        />
+                        <p className="legend">Posterior Izquiera</p>
+                      </div>
+                      <div>
+                        <img
+                          src={api + JSON.parse(event.images).url6}
+                          alt={api + JSON.parse(event.images).url6}
+                        />
+                        <p className="legend">Posterior Derecha</p>
+                      </div>
+                      <div>
+                        <img
+                          src={api + JSON.parse(event.images).url7}
+                          alt={api + JSON.parse(event.images).url7}
+                        />
+                        <p className="legend">Toma frontal de la unidad</p>
+                      </div>
+                      <div>
+                        <img
+                          src={api + JSON.parse(event.images).url8}
+                          alt={api + JSON.parse(event.images).url8}
+                        />
+                        <p className="legend">Toma posterior de la unidad</p>
+                      </div>
+                      <div>
+                        <img
+                          src={api + JSON.parse(event.images).url9}
+                          alt={api + JSON.parse(event.images).url9}
+                        />
+                        <p className="legend">Luces delanteras</p>
+                      </div>
+                      <div>
+                        <img
+                          src={api + JSON.parse(event.images).url10}
+                          alt={api + JSON.parse(event.images).url10}
+                        />
+                        <p className="legend">Luces posteriores</p>
+                      </div>
+                      <div>
+                        <img
+                          src={api + JSON.parse(event.images).url11}
+                          alt={api + JSON.parse(event.images).url11}
+                        />
+                        <p className="legend">Valvula interna</p>
+                      </div>
+                      <div>
+                        <img
+                          src={api + JSON.parse(event.images).url12}
+                          alt={api + JSON.parse(event.images).url12}
+                        />
+                        <p className="legend">Eslingas Colocadas</p>
+                      </div>
+                    </Carousel>
+                  ) : (
+                    <Carousel dynamicHeight={20} thumbWidth={35}>
+                      <div>
+                        <img
+                          src={api + JSON.parse(event.images).url1}
+                          alt={api + JSON.parse(event.images).url1}
+                        />
+                        <p className="legend">Selfie del Conductor</p>
+                      </div>
+                      <div>
+                        <img
+                          src={api + JSON.parse(event.images).url2}
+                          alt={api + JSON.parse(event.images).url2}
+                        />
+                        <p className="legend">Extintor</p>
+                      </div>
+                      <div>
+                        <img
+                          src={api + JSON.parse(event.images).url3}
+                          alt={api + JSON.parse(event.images).url3}
+                        />
+                        <p className="legend">Delantero Izquierdo</p>
+                      </div>
+                      <div>
+                        <img
+                          src={api + JSON.parse(event.images).url4}
+                          alt={api + JSON.parse(event.images).url4}
+                        />
+                        <p className="legend">Delantero Derecho</p>
+                      </div>
+                      <div>
+                        <img
+                          src={api + JSON.parse(event.images).url5}
+                          alt={api + JSON.parse(event.images).url5}
+                        />
+                        <p className="legend">Posterior Izquiera</p>
+                      </div>
+                      <div>
+                        <img
+                          src={api + JSON.parse(event.images).url6}
+                          alt={api + JSON.parse(event.images).url6}
+                        />
+                        <p className="legend">Posterior Derecha</p>
+                      </div>
+                      <div>
+                        <img
+                          src={api + JSON.parse(event.images).url7}
+                          alt={api + JSON.parse(event.images).url7}
+                        />
+                        <p className="legend">Toma frontal de la unidad</p>
+                      </div>
+                      <div>
+                        <img
+                          src={api + JSON.parse(event.images).url8}
+                          alt={api + JSON.parse(event.images).url8}
+                        />
+                        <p className="legend">Toma posterior de la unidad</p>
+                      </div>
+                      <div>
+                        <img
+                          src={api + JSON.parse(event.images).url9}
+                          alt={api + JSON.parse(event.images).url9}
+                        />
+                        <p className="legend">Luces delanteras</p>
+                      </div>
+                      <div>
+                        <img
+                          src={api + JSON.parse(event.images).url10}
+                          alt={api + JSON.parse(event.images).url10}
+                        />
+                        <p className="legend">Luces posteriores</p>
+                      </div>
+                      <div>
+                        <img
+                          src={api + JSON.parse(event.images).url11}
+                          alt={api + JSON.parse(event.images).url11}
+                        />
+                        <p className="legend">Valvula interna</p>
+                      </div>
+                    </Carousel>
+                  )}
                 </div>
               </div>
             </div>
