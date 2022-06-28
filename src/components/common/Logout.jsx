b44de890b240365739a2b95bb8fed2eee8605d75
Eslim Daga <esliim9811@gmail.com>
2022-06-28 17:40:21 -0500

    Apply prettier

--- src/components/common/Logout.jsx
+++ src/components/common/Logout.jsx
@@ -2,11 +2,11 @@ import { useEffect } from "react";
 import cache from "../../helpers/cache";
 
 const Logout = () => {
-  useEffect(() => {
-    cache.removeItem("user");
-    window.location.href = "/";
-  }, [])
-  return null;
-}
+	useEffect(() => {
+		cache.removeItem("user");
+		window.location.href = "/";
+	}, []);
+	return null;
+};
 
 export default Logout;
