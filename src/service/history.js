3a2968bc7f431b7877738f7779be874e327b79c9
Eslim Daga <esliim9811@gmail.com>
2022-07-06 15:41:43 -0500

    add get event with group report

--- src/service/history.js
+++ src/service/history.js
@@ -3,31 +3,47 @@ import cache from "../helpers/cache";
 import { api } from "../constants/global";
 
 export const getUnits = async () => {
-  const token = cache.getItem("user").token;
-  const response = await axios.get(`${api}/control/web/api/get-units/`,{
-    headers: {
-      "Authorization": `JWT ${token}`
-    }
-  });
-  return response.data;
-}
+	const token = cache.getItem("user").token;
+	const response = await axios.get(`${api}/control/web/api/get-units/`, {
+		headers: {
+			Authorization: `JWT ${token}`,
+		},
+	});
+	return response.data;
+};
 
 axios.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response.status === 401) {
-      cache.removeItem("user");
-      window.location = "/login";
-    }
-  }
+	response => response,
+	error => {
+		if (error.response.status === 401) {
+			cache.removeItem("user");
+			window.location = "/login";
+		}
+	}
 );
 
-export const getSearchEvents = async(initial_date,final_date,unit_name) => {
-  const token = cache.getItem("user").token;
-  const response = await axios.get(`${api}/control/web/api/search-events/${initial_date}/${final_date}/${unit_name}/`,{
-    headers: {
-      "Authorization": `JWT ${token}`
-    }
-  });
-  return response.data;
-}
+export const getSearchEvents = async (initial_date, final_date, unit_name) => {
+	const token = cache.getItem("user").token;
+	const response = await axios.get(
+		`${api}/control/web/api/search-events/${initial_date}/${final_date}/${unit_name}/`,
+		{
+			headers: {
+				Authorization: `JWT ${token}`,
+			},
+		}
+	);
+	return response.data;
+};
+
+export const getSearchEventsWithGrouping = async (initial_date, final_date) => {
+	const token = cache.getItem("user").token;
+	const response = await axios.get(
+		`${api}/control/web/api/search-events-with-grouping/${initial_date}/${final_date}/`,
+		{
+			headers: {
+				Authorization: `JWT ${token}`,
+			},
+		}
+	);
+	return response.data;
+};
